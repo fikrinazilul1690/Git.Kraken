0cbe1d180fb291ddc275168cd8e88057e13ffb78
Achmad Nazilul Fikri <fikrinazilul@gmail.com>
2020-10-01 20:39:18 +0700

    Revisi script.js

--- CERC-Web/Pertemuan 1/script.js
+++ CERC-Web/Pertemuan 1/script.js
@@ -2,13 +2,13 @@ const challenge1 = () => {
     axios
         .get("https://jsonplaceholder.typicode.com/comments")
         .then((res) => {
-            const filterData = res.data.filter(value => {
+            const filterComments = res.data.filter(value => {
                 return value.postId == 8;
             });
             console.log({
                 Nama: "Achmad Nazilul Fikri",
-                dataLength: Object.keys(filterData).length,
-                data: filterData,
+                dataLength: Object.keys(filterComments).length,
+                data: filterComments,
             });
         });
 };
