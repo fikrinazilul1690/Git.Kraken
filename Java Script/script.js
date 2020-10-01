691bc003ea91de4818063811fff2a8251cb429e6
Achmad Nazilul Fikri <fikrinazilul@gmail.com>
2020-10-01 11:42:44 +0700

    Challenge CERC WEB Pertemuan 1

---
+++ Java Script/script.js
@@ -0,0 +1,17 @@
+console.log("Achmad Nazilul Fikri");
+const challenge1 = () => {
+    axios
+        .get("https://jsonplaceholder.typicode.com/posts/8/comments")
+        .then((res) => {
+            console.log(res.data);
+        });
+
+};
+
+const challenge2 = () => {
+    axios
+        .get("https://jsonplaceholder.typicode.com/posts?userId-8")
+        .then((res) => {
+            console.log(res.data);
+        });
+};
