9614ae5c96b521af09315aaee604c73277456f78
Achmad Nazilul Fikri <fikrinazilul@gmail.com>
2020-10-01 20:31:51 +0700

    Revisi Challenge 1 & 2

--- CERC-Web/Pertemuan 1/script.js
+++ CERC-Web/Pertemuan 1/script.js
@@ -1,17 +1,29 @@
-console.log("Achmad Nazilul Fikri");
 const challenge1 = () => {
     axios
-        .get("https://jsonplaceholder.typicode.com/posts/8/comments")
+        .get("https://jsonplaceholder.typicode.com/comments")
         .then((res) => {
-            console.log(res.data);
+            const filterData = res.data.filter(value => {
+                return value.postId == 8;
+            });
+            console.log({
+                Nama: "Achmad Nazilul Fikri",
+                dataLength: Object.keys(filterData).length,
+                data: filterData,
+            });
         });
-
 };
 
 const challenge2 = () => {
     axios
-        .get("https://jsonplaceholder.typicode.com/posts?userId=8")
+        .get("https://jsonplaceholder.typicode.com/posts")
         .then((res) => {
-            console.log(res.data);
+            const filterData = res.data.filter( value =>{
+                return value.userId == 8;
+            });            
+            console.log({
+                Nama: "Achmad Nazilul Fikri",
+                dataLength: Object.keys(filterData).length,
+                data: filterData,
+            });
         });
 };
